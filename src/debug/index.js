5ae56ed8c7e6bbcd5b11500c5b9c4b0636df54dd
Walter <walter.salas@me.com>
2020-06-16 10:56:21 -0500

    debugger

---
+++ src/debug/index.js
@@ -0,0 +1,13 @@
+var a = 'hello';
+function hello () {
+    let b = 'hello world'
+    const c = 'HELLO WORLD!!!!'
+    if (true) {
+        let d = 'HELLO WORLD!!!! MADA FAKA!!'
+        debugger // esto pausa la aplicacion y podemos entender que esta pasando en este punto
+    }
+}
+
+hello();
+
+
